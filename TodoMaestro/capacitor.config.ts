221ae412f2e1a10a913fd34e52bf9ce06281f1ef
Judi <judithavalos6@gmail.com>
2024-10-23 21:58:19 -0300

    Primer commit Ionic

---
+++ TodoMaestro/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TodoMaestro',
+  webDir: 'dist'
+};
+
+export default config;
